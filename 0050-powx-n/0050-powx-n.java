a88e35ddeefd12d093b406a0cf4e2601533bc933
Prakarsh Kaushik <63556277+Prakarsh-Kaushik@users.noreply.github.com>
2024-04-13 21:53:38 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (47.61%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,11 @@
+class Solution {
+    public double myPow(double x, int n) {
+        // double result = 1;
+        // while(n!=0){
+        //     result = result * x;
+        //     n--;
+        // }
+        // return result;
+    return Math.pow(x,n);
+    }
+}
